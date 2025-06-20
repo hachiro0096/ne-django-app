44a8ae971bf52be3949e855867da0f45f220f8ab
hachiro <23ne037@ms.dendai.ac.com>
2025-06-20 13:49:23 +0900

    最初のコミット

---
+++ djangosnippets/helloworld/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from helloworld.models import Helloworld
+
+# Register your models here.
+admin.site.register(Helloworld)
