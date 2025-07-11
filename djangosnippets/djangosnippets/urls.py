3a1d921d500178764bf060304e569692975cc0bd
y030317 <23ne023@ms.dendai.ac.jp>
2025-07-11 13:05:28 +0900

    test

--- djangosnippets/djangosnippets/urls.py
+++ djangosnippets/djangosnippets/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('helloworld.urls')),           # ← ここだけ
+    path('helloworld/', include('helloworld.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # ← これを追加！
 ]
