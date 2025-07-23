34cff78274ec42ba03db7ecc33a937bb6c156bee
y030317 <23ne023@ms.dendai.ac.jp>
2025-07-23 14:54:35 +0900

    test

--- djangosnippets/helloworld/urls.py
+++ djangosnippets/helloworld/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+
+
 
 urlpatterns = [
     path('', views.top, name='top'),
@@ -18,6 +21,7 @@ urlpatterns = [
 
     path('profile/edit/', views.profile_edit, name='profile_edit'),
 
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
 
 
 
